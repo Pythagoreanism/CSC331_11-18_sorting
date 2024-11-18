1c2f58eb63201ffb70b6ef78fd44ed43bf58c87e
Jason Palma <jasonpalma16@gmail.com>
2024-11-17 23:35:55 -0500

    Define printArray

--- main.cpp
+++ main.cpp
@@ -11,6 +11,17 @@ int* quickSort(int[], int);
 
 
 int main(int argc, char* argv[]) {
+    const int SIZE = 10;
+    int myArray[SIZE] = { 1, 4, 5, 1, 34, 60 };
+
+    printArray(myArray, SIZE);
 
     return 0;
+}
+
+
+void printArray(int arr[], int size) {
+    for (unsigned int i = 0; i < size; i++) {
+        cout << i + 1 << ": " << arr[i] << endl;
+    }
 }
