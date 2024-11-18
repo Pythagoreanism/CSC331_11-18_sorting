7a353e6b18d4630ca83f402d1b7607136004cea5
Jason Palma <jasonpalma16@gmail.com>
2024-11-17 23:45:39 -0500

    Create and define initArray function
    The function will be used to initialize an array with random numbers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 
 void printArray(int[], int);
+void initArray(int[], int);
 int* selectionSort(int[], int);
 int* insertionSort(int[], int);
 int* heapSort(int[], int);
@@ -11,9 +14,12 @@ int* quickSort(int[], int);
 
 
 int main(int argc, char* argv[]) {
-    const int SIZE = 10;
-    int myArray[SIZE] = { 1, 4, 5, 1, 34, 60 };
+    srand(time(NULL)); // Initialize random seed
 
+    const int SIZE = 10;
+    int myArray[SIZE]; // Initializes array elements to 0
+
+    initArray(myArray, SIZE);
     printArray(myArray, SIZE);
 
     return 0;
@@ -23,5 +29,11 @@ int main(int argc, char* argv[]) {
 void printArray(int arr[], int size) {
     for (unsigned int i = 0; i < size; i++) {
         cout << i + 1 << ": " << arr[i] << endl;
+    }
+}
+
+void initArray(int arr[], int size) {
+    for (unsigned int i = 0; i < size; i++) {
+        arr[i] = rand() % 101;
     }
 }
