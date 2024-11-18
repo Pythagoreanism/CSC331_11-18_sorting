a647f68d8fbbe0117689ecf9e785cd9b662e5716
Jason Palma <jasonpalma16@gmail.com>
2024-11-17 23:34:35 -0500

    Add function prototypes

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+void printArray(int[], int);
+int* selectionSort(int[], int);
+int* insertionSort(int[], int);
+int* heapSort(int[], int);
+int* mergeSort(int[], int);
+int* quickSort(int[], int);
+
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
