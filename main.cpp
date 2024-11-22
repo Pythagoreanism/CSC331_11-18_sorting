da56541f33caa18c5d1d961a91fadf5dc8c25fd7
Jason Palma <jasonpalma16@gmail.com>
2024-11-21 22:58:08 -0500

    Create and define functions to initialize array
    There are four different ways to fill the array for testing.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 
 void printArray(int[], int);
-void initArray(int[], int);
+void initArray1(int[], int); // a. Completely random
+void initArray2(int[], int); // b. Almost sorted in ascending order
+void initArray3(int[], int); // c. Almost reversed similar to b.
+void initArray4(int[], int); // d. Sorted except last 10%
 int* selectionSort(int[], int);
 int* insertionSort(int[], int);
 int* heapSort(int[], int);
@@ -16,11 +19,16 @@ int* quickSort(int[], int);
 int main(int argc, char* argv[]) {
     srand(time(NULL)); // Initialize random seed
 
-    const int SIZE = 10;
+    const int SIZE = 30;
     int myArray[SIZE]; // Initializes array elements to 0
 
-    initArray(myArray, SIZE);
+    //initArray(myArray, SIZE);
+    //initArray2(myArray, SIZE);
+    //initArray3(myArray, SIZE);
+    initArray4(myArray, SIZE);
+    
     printArray(myArray, SIZE);
+
 
     return 0;
 }
@@ -32,8 +40,51 @@ void printArray(int arr[], int size) {
     }
 }
 
-void initArray(int arr[], int size) {
+
+void initArray1(int arr[], int size) {
     for (unsigned int i = 0; i < size; i++) {
         arr[i] = rand() % 101;
+    }
+}
+
+
+void initArray2(int arr[], int size) {
+    int next = 0;
+
+    for (unsigned int i = 0; i < size; i++) { // Fill array w/ randomly incremented numbers
+        next = next + ((rand() % 5) + 1); // Not incrementing too much
+        arr[i] = next;
+    }
+
+    for (unsigned int i = 0; i < size * .1; i++) { // Make 10% random
+        arr[rand() % size + 1] = rand() % 1024; // Make random element random
+    }
+}
+
+
+void initArray3(int arr[], int size) {
+    int next = 0;
+
+    for (int i = size - 1; i >= 0; i--) { // Fill array w/ randomly incremented numbers
+        next = next + ((rand() % 5) + 1); // Not incrementing too much
+        arr[i] = next;
+    }
+
+    for (unsigned int i = 0; i < size * .1; i++) {
+        arr[rand() % size + 1] = rand() % 1024;
+    }
+}
+
+
+void initArray4(int arr[], int size) {
+    int next = 0;
+
+    for (unsigned int i = 0; i < size; i++) {
+        next = next + ((rand() % 5) + 1);
+        arr[i] = next;
+    }
+
+    for (unsigned int i = size - (size * .1); i < size; i++) {
+        arr[i] = rand() % 1024;
     }
 }
